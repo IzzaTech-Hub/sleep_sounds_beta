b29b6d828360606c0c8f7ad8caf21f001de19bc5
Syed Jamal Ahmad <syedjamal969@gmail.com>
2025-07-14 15:44:10 +0500

    SDK 35

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
-    // DeviceOrientation.landscapeRight,
+    // DeviceOrientation.landscapeRight, 
   ]);
   await initializeDefault();
   runApp(const MyApp());
